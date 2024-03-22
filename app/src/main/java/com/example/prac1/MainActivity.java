2d1040cfc6d7bcba5dd4778de1cbce660a910eb0
Maksim <maksim.krestin2003@gmail.com>
2024-03-22 22:53:30 +0300

    prac 6

--- app/src/main/java/com/example/prac1/MainActivity.java
+++ app/src/main/java/com/example/prac1/MainActivity.java
@@ -1,26 +1,39 @@
 package com.example.prac1;
 
+import android.app.NotificationChannel;
+import android.app.NotificationManager;
+import android.content.Intent;
 import android.os.Bundle;
 import android.util.Log;
 import android.widget.Toast;
 
-import androidx.activity.result.ActivityResultLauncher;
-import androidx.activity.result.contract.ActivityResultContracts;
 import androidx.appcompat.app.AppCompatActivity;
 
 import com.example.prac1.databinding.ActivityMainBinding;
 
 public class MainActivity extends AppCompatActivity {
 
+    static final String CHANNEL_ID = "my_channel";
+    static final int NOTIFICATION_ID = 101;
     ActivityMainBinding binding;
     private static final String TAG = "ACTIVITY_LIFECYCLE";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        Intent intent = new Intent(this, MyService.class);
+        startForegroundService(intent);
         binding = ActivityMainBinding.inflate(getLayoutInflater());
         setContentView(binding.getRoot());
 
+        NotificationChannel channel = new NotificationChannel(
+                CHANNEL_ID,
+                CHANNEL_ID,
+                NotificationManager.IMPORTANCE_DEFAULT);
+        channel.setDescription("");
+
+        NotificationManager notificationManager = getSystemService(NotificationManager.class);
+        notificationManager.createNotificationChannel(channel);
     }
 
     @Override
